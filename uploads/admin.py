8c390065531e98e061be4a9ad4a3bde85649a644
Lucas Reis <reisdossousalucasedson@gmail.com>
2025-11-14 18:03:43 -0300

    feat(uploads/admin): add useful info to list_display and list_filter

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from uploads.models import Image, Video
+from django.utils.html import format_html
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['uuid', 'processed']
+    list_display = ['post', 'kind', 'link', 'processed']
+    list_filter = ('post', 'kind', 'processed')
+
+    def link(self, obj):
+        return format_html('<a href="{}" target="_blank">link</a>', obj.image.url)
 
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ['uuid', 'processed']
+    list_display = ['post', 'link', 'processed']
+    list_filter = ('post', 'processed')
+
+    def link(self, obj):   
+        return format_html('<a href="{}" target="_blank">link</a>', obj.video.url)
